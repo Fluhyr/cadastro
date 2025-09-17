e708690530e33ad45d187efc319c1886d3bf26cb
Fluhyr <lucas.fluh@estudante.ifms.edu.br>
2025-09-17 14:28:40 -0400

    commit 2222

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-//        Para CPF
-//
-//      /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/
-//
-//
-//      Para CNPJ
-//    /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/
-//
-//      Para ambos ao mesmo tempo
-//
-//      /(^\d{3}\.\d{3}\.\d{3}\-\d{2}$)|(^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$)/
+/*
+    ETAPAS:
+
+    1- se usuario estiver com caracteres (pelo menos 5) msg de erro e icone de erro
+    2- Email - verificador email
+    3- senha - min 6 caracteres
+    4- confirmar senha == senha
+    5- verificador CPF
+    6- verificador CNPJ
+    7- tirar o display none do cpf ou cnpj selector
+
+    --------------
+
+    EXTRA
+
+    pagina de login com BD
+*/
+
+//etapa 7
+
+
+
+
+
+
+
+/* VERIFICAÇÃO FINAL CADASTRO */
+document.getElementById("btEnviar").onclick = function() {
+    vericação = 1;
+    if(vericação == 1){
+        alert("teste")
+    }
+}
