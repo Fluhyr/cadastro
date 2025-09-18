dd4059d4af1d3a4c0253fed836618ad544b86604
Fluhyr <lucas.fluhr@estudante.ifms.edu.br>
2025-09-18 12:27:38 -0400

    Site Versão 1.3

--- script.js
+++ script.js
@@ -16,6 +16,19 @@ var verificacaoSenha = false;
 //---------------------------------------------------------------------------------------------
 /* VERIFICAR SENHA */
 
+//ESTILO DO INPUT QUANDO VERIFICAÇÃO FALSA
+var estiloErro = {
+    backgroundImage: 'url(content/erro.png)',
+    backgroundSize: '20px',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: 'rgba(194, 34, 34, 0.164)',
+    backgroundPosition: '290px'
+}
+var estiloAcerto = {
+    backgroundImage: 'none',
+    backgroundColor: 'rgb(238, 238, 238)',
+}
+
 function VerificadorSenha(){
     var senhaUsuario = document.getElementById("inputSenha");
     var confirmarSenha = document.getElementById("inputConfirmarsenha");
@@ -23,13 +36,24 @@ function VerificadorSenha(){
     if(senhaUsuario.value.length >= 6){
         if(senhaUsuario.value == confirmarSenha.value){
         verificacaoSenha = true;
-        alert("senha igual")
+            Object.assign(document.getElementById("inputSenha").style, estiloAcerto);
+            Object.assign(document.getElementById("inputConfirmarsenha").style, estiloAcerto);
+            document.getElementById("erroConfirmarsenha").style.display = 'none';
+            document.getElementById("erroSenha").style.display = 'none';
         }else{
-            alert("senha diferente")
+            Object.assign(document.getElementById("inputSenha").style, estiloErro);
+            Object.assign(document.getElementById("inputConfirmarsenha").style, estiloErro);
+            document.getElementById("erroConfirmarsenha").style.display = 'block';
         }
     }
     else{
-        alert("Senha menor q 6 digitos")
+        //MENSAGEM DE ERRO MT FODA
+        
+        Object.assign(document.getElementById("inputSenha").style, estiloErro);
+        document.getElementById("erroSenha").style.display = 'block';
+
+        //alert
+
     }
 }
 //---------------------------------------------------------------------------------------------
